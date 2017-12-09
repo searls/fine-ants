0e84f46cb374b8aab6a6e29085f12837f32e3f00
Justin Searls <searls@gmail.com>
2017-12-09 17:37:40 -0500

    sometimes they a/b test. yay

--- lib/fine_ants/adapters/zillow.rb
+++ lib/fine_ants/adapters/zillow.rb
@@ -13,14 +13,29 @@ module FineAnts
 
       def download
         visit "https://www.zillow.com/homedetails/total_nonsense/#{@user}_zpid/?fullpage=true"
-        zestimate = find('.estimates .home-summary-row:nth-child(2) span:nth-child(2)').text
+        zestimate = find_first(
+          '.estimates .home-summary-row:nth-child(2) span:nth-child(2)',
+          '.zestimate.primary-quote'
+        ).text.match(/(\$.*)/)[1]
+
         [{
           :adapter => :zillow,
           :user => @user,
           :id => @user,
-          :name => find('.addr h1').text,
+          :name => find_first('.addr h1', '.hdp-home-header-st-addr').text,
           :amount => BigDecimal.new(zestimate.gsub(/[\$,]/,''))
         }]
+      end
+
+      private
+
+      def find_first(*locators)
+        locators.each do |locator|
+          begin
+            return find(locator)
+          rescue Capybara::ElementNotFound
+          end
+        end
       end
     end
   end
