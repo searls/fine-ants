6556a08ede778d6b5259d21baa699761442563bf
Justin Searls <searls@gmail.com>
2019-07-21 15:35:29 -0400

    bring it down a bit

--- lib/fine_ants/capybara_setup.rb
+++ lib/fine_ants/capybara_setup.rb
@@ -9,4 +9,4 @@ Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 Capybara.default_driver = :selenium
-Capybara.default_max_wait_time = 30
+Capybara.default_max_wait_time = 20
