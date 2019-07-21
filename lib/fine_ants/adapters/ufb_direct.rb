2edc64a84ab524ef4352dbaad32a4d2ca6e0779b
Justin Searls <searls@gmail.com>
2019-07-21 15:18:42 -0400

    add UFB Direct
    it is so slow that i needed to increase max wait time to 30 and add
    like 5 second sleeps so it wouldn't read empty angular bindings lololol

---
+++ lib/fine_ants/adapters/ufb_direct.rb
@@ -0,0 +1,61 @@
+require "bigdecimal"
+
+module FineAnts
+  module Adapters
+    class UfbDirect
+      def initialize(credentials)
+        @user = credentials[:user]
+        @password = credentials[:password]
+      end
+
+      def login
+        visit "https://onlinebanking.ufbdirect.com/auth/login"
+        sleep 2
+        click_link "Login"
+        sleep 2
+        fill_in "Username", with: @user
+        fill_in "Password", with: @password
+        sleep 1
+        click_button "Login"
+        begin
+          find ".multi-factor"
+          click_button "btn-sms"
+          return false
+        rescue Capybara::ElementNotFound
+          verify_login!
+          return true
+        end
+      end
+
+      def two_factor_response(answer)
+        fill_in "access-code-entry", with: answer
+        click_button "Continue"
+        verify_login!
+      end
+
+      def download
+        sleep 5
+        rows = all(".details-container")
+        rows.map { |row|
+          {
+            adapter: :ufb_direct,
+            user: @user,
+            id: row.find(".details-container__account-name").text,
+            name: row.find(".details-container__account-name").text,
+            amount: BigDecimal(
+              row.find(".details-container__account-amount").text.match(/\$(.*)$/)[1].delete(",")
+            ),
+          }
+        }
+      end
+
+      private
+
+      def verify_login!
+        find ".details-container"
+      rescue Capybara::ElementNotFound
+        raise FineAnts::LoginFailedError.new
+      end
+    end
+  end
+end
